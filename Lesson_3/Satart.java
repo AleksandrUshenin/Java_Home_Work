ac97e76ff6e1993d51eb482a0ac71173adecd8e5
Aleksandr Ushenin <sasha0625@mail.ru>
2022-10-09 17:05:03 +0300

    выполнено дз3

--- Lesson_3/Satart.java
+++ Lesson_3/Satart.java
@@ -1,4 +1,5 @@
 package Lesson_3;
+import java.util.*;
 
 import java.util.Arrays;
 
@@ -7,6 +8,14 @@ import java.util.Arrays;
  */
 public class Satart {
     public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 51; i++) {
+            list.add(i);
+        }
+        Ex1(list);
+        System.out.println(list);
+        Ex2(list);
+
         int[] arr = {8, 4, 1, 5, 7, 2, 6, 3};
         PrintArr(arr);
         PrintArr(SorterArr(arr));
@@ -16,12 +25,31 @@ public class Satart {
         PrintArr(SorterArr(arr2));
         
     }
+    private static void Ex1(List<Integer> list){
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) % 2 ==0){
+                list.remove(i);
+            }
+        }
+    }
+    private static void Ex2(List<Integer> list){
+        int min = list.get(0);
+        int max = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (min > list.get(i)){
+                min = list.get(i);
+            }
+            if (max < list.get(i)){
+                max = list.get(i);
+            }
+        }
+        double midle = (double)max / list.size();
+    }
+
     private static int[] SorterArr(int[] arr){
         int len1 = arr.length;
-        //if (len1 % 2 != 0){
-        //    len 
-        //}
         int len2 = 2;
+        int len3 = len1;
         int[][] arr2 = new int[len1][];
         int[][] res;
 
@@ -35,40 +63,71 @@ public class Satart {
         }
 
         int index1 = 0;
-        int index2 = 0;
+        int index2 = 1;
         int index3 = 0;
         int count = 1;
-        len1 /= 2;
+        if (len1 % 2 != 0){
+            len1 /= 2 + 1;
+        }
+        else{
+            len1 /= 2;
+        }
         res = new int[len1][];
-        while(count != arr.length / 2){//(arr2[0].length == arr.length){
-            //res[index3] = new int[len2 * 2];
+        while(count != arr.length / 2){
 
-            res[index3] = ex3(arr2[index1], arr2[index1 + 1]);
+            res[index3] = ex3(arr2[index1], arr2[index2]);
             
             if (index3 != len1 - 1){
                 index3++;
             }
             else{
-                len1 /= 2;
+                len3 = len1;
+                if (len1 % 2 != 0){
+                    len1 = len1 / 2 + 1;
+                }
+                else{
+                    len1 /= 2;
+                }
                 len2 *= 2;
                 arr2 = Arrays.copyOf(res, arr.length);
                 res = new int[len1][];
                 count++;
                 index3 = 0;
+                index2 = 1;
                 index1 = 0;
                 continue;
             }
             if(index1 != arr2.length){//(index1 != arr.length)
                 index1 += 2;
-                if (index2 != arr2[index1].length){
-                    index2 += 2;
+                index2 += 2;
+                if (index2 >= len3){
+                    len3 = len1;
+                    res[index3] = arr2[index2 - 1];
+                    if (len1 % 2 != 0){
+                        len1 = len1 / 2 + 1;
+                    }
+                    else{
+                        len1 /= 2;
+                    }
+                    len2 *= 2;
+                    arr2 = Arrays.copyOf(res, arr.length);
+                    res = new int[len1][];
+                    count++;
+                    index3 = 0;
+                    index2 = 1;
+                    index1 = 0;
+                    continue;
                 }
-                else{
-                    index2 = 0;
-                }
+                // if (index2 != arr2[index1].length){
+                //     index2 += 3;
+                // }
+                // else{
+                //     index2 = 0;
+                // }
             }
             else{
                 index1 = 0;
+                index2 = 0;
             }
         }
         //PrintArr(arr2[0]);
@@ -95,6 +154,9 @@ public class Satart {
                 res[i + 1] = right[index2];
                 break;
             }
+        }
+        if (index2 != right.length){
+            res[res.length - 1] = right[right.length - 1];
         }
         return res;
     }
