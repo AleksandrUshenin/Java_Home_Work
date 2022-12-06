2b85606fa7bdb25586128dc3c2a8851b1b4ebddb
Aleksandr Ushenin <sasha0625@mail.ru>
2022-12-06 22:28:23 +0300

    итоговый проект

---
+++ Exep3/Start.java
@@ -0,0 +1,12 @@
+package Exep3;
+
+/**
+ * Start
+ */
+public class Start {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
+    }
+    
+}
